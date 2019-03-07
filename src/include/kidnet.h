7d53e727cae0ab05429ec7aa6bc3f509b7943df5
Kawaharasouta <kawahara6514@gmail.com>
2019-03-07 08:57:43 +0000

    save 20190307

--- src/include/kidnet.h
+++ src/include/kidnet.h
@@ -2,6 +2,6 @@
 #define _KIDNET_H_
 
 #define INTEL_KIDNET_ETHERNET_DEVICE(device_id) {\
-				PCI_DEVICE(PCI_VENDOR_ID_ID_INTEL, device_id)}
+				PCI_DEVICE(PCI_VENDOR_ID_INTEL, device_id)}
 
 #endif /* _KIDNET_H_ */
--- src/kidnet.c
+++ src/kidnet.c
@@ -3,39 +3,57 @@
 #include<linux/kernel.h>
 #include<linux/pci.h>
 
+#include"include/kidnet.h"
+
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Kawaharasouta <kawahara6514@gmail.com>");
 MODULE_DESCRIPTION("network module");
 
 static char *kidnet_msg = "module [kidnet]:";
-char *kidnet_driver_name = "kidnet";
+static char kidnet_driver_name[] = "kidnet";
 
+static struct pci_device_id kidnet_pci_tbl[] = {
+	INTEL_KIDNET_ETHERNET_DEVICE(0x1528), /* X540-t1, X540-t2 */
+	/* required last entry */
+	{0,}
+};
 
 
 static int 
 kidnet_init_one(struct pci_dev *pdev, const struct pci_device_id *ent) {
-
-
+	struct net_device *netdev;
 }
+static void 
+kidnet_remove_one(struct pci_dev *pdev) {
+	struct net_device *dev = pci_get_drvdata(pdev);
 
+	flush_sheduled_work();
+
+	unregister_netdev(dev);
+
+
+	pci_disable_device(pdev);
+}
 
 
 #if 1
 //struct pci_device_id pci_device_id;
 struct pci_driver kidnet_driver = {
-	.name = kidnet_name,
-	.id_table =  // The device list this driver support.
-	.probe = kidnet_init_one // It is called at device detection if it is a prescribed driver, or at (possibly) modprobe otherwise.
-	.remove =  // It is called at device unload.
-#ifdef CONFIG_PM // When the power control is ON.
+	.name = kidnet_driver_name,
+	.id_table = kidnet_pci_tbl, /* The device list this driver support. */
+	.probe = kidnet_init_one, /* It is called at device detection if it is a prescribed driver, or at (possibly) modprobe otherwise. */
+	.remove =  kidnet_remove_one, /* It is called at device unload. */
+#if 0
+#ifdef CONFIG_PM /*When the power control is ON. */
 	.suspend =
 	.resume = 
 #endif /* CONFIG_PM */
-}
+#endif
+};
 #endif
 
 
-#if 1
+#if 0
 static const struct net_device_ops kidnet_ops = {
 	.ndo_open =
 	.ndo_stop = 
@@ -52,13 +70,13 @@ static const struct net_device_ops kidnet_ops = {
 	.ndo_poll_controller =
 	.ndo_fix_features =
 	.ndo_set_features =
-}
+}:
 #endif
 
 
 static int 
 kidnet_init(void) {
-	printk(KERN_ALERT "%s loading kidnet.\n", pci_exp_msg);
+	printk(KERN_ALERT "%s loading kidnet.\n", kidnet_msg);
 	return 0;
 
 	int ret;
@@ -68,10 +86,9 @@ kidnet_init(void) {
 }
 static void 
 kidnet_exit(void) {
-	printk(KERN_ALERT "%s kidnet bye.\n", pci_exp_msg);
+	printk(KERN_ALERT "%s kidnet bye.\n", kidnet_msg);
 
-	int ret;
-	ret = pci_unregister_driver(&kidnet_driver);
+	pci_unregister_driver(&kidnet_driver);
 
 	return;
 }
